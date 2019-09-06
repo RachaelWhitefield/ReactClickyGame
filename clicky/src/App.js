b9e07b1e52c93db3c440abc364b42ad778106c41
Fabulous_Rachael <rachael.whitefield@gmail.com>
2019-09-06 15:39:12 -0600

    forgot to commit changes from last week

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
-import Wrapper from "./components/Wrapper";
+import FriendCard from "./components/FriendCard/FriendCard";
+import Wrapper from "./components/Wrapper/Wrapper";
 import friends from "./friends.json";
-// import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import "./App.css";
 
 class App extends Component {
   state = {
+    clickedFriends: [],
     friends,
     score: 0,
-    hightscore: 0
+    highscore: 0
   };
 
+  clickCount = (id) => {
+    let clickedFriendsCopy = [...this.state.clickedFriends];
+    
+    console.log(clickedFriendsCopy);
+    if (clickedFriendsCopy.includes(id)) {
+      console.log("They already clicked this friend")
+      this.gameOver();
+    } else {
+      clickedFriendsCopy.push(id);
+      this.setState({
+        score: this.state.score + 1,
+        clickedFriends: clickedFriendsCopy});
+      }
+  }
+
   gameOver = () => {
-    if (this.state.score > this.state.hghscore) {
-      this.setState({highscore: this.state.score}, function() {
-        console.log(this.state.highscore);
-      });
+    console.log(this.state.score + "score")
+    console.log(this.state.highscore + "highscore");
+    if (this.state.score > this.state.highscore) {
+      this.setState({highscore: this.state.score})
+        console.log(this.state.highscore + "highscore");
     }
-    this.state.cards.forEach(card => {
-      card.count = 0;
+    this.state.friends.forEach(friend => {
+      friend.count = 0;
     });
     alert(`Game Over \nscore: ${this.state.score}`);
     this.setState({score: 0});
@@ -28,7 +45,7 @@ class App extends Component {
   render() {
     return (
       <Wrapper>
-      <header score={this.state.score} highscore={this.state.hightscore}><strong>Clicky Game</strong></header>
+      <Header score={this.state.score} highscore={this.state.highscore}><strong>Clicky Game</strong></Header>
         {this.state.friends.map(friend => (
           <FriendCard
             clickCount={this.clickCount}
